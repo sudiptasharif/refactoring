16a9d4c45613aba031c60321360c636aaac45c8a
Sharif <sudiptasharif@gmail.com>
2023-03-18 23:09:46 -0500

    Changing the local function variable format with declared function format() to remove local temporary variables

--- chp1/chp1.js
+++ chp1/chp1.js
@@ -28,11 +28,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     // add volume credits
@@ -88,4 +83,11 @@ function volumeCreditsFor(aPerformance, aPlay) {
   return result;
 }
 
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
+}
 console.log(statement(invoices[0], plays));
